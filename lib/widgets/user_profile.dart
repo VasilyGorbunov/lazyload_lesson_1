cc88fe17865d3bf5fef8e2c6205927cdd6e86a35
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-06-08 19:09:45 +0300

    урок 21 end

--- lib/widgets/user_profile.dart
+++ lib/widgets/user_profile.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class UserProfile extends StatelessWidget {
-  const UserProfile({super.key});
+  UserProfile({super.key});
+
+  List<MenuRowData> firstMenuRows = [
+    MenuRowData(Icons.bookmark_border_outlined, 'Избранное'),
+    MenuRowData(Icons.call_outlined, 'Звонки'),
+    MenuRowData(Icons.computer_outlined, 'Устройства'),
+    MenuRowData(Icons.chat_bubble_outline, 'Папка с чатами'),
+  ];
+
+  List<MenuRowData> secondMenuRows = [
+    MenuRowData(Icons.notifications, 'Уведомления и звуки'),
+    MenuRowData(Icons.privacy_tip_outlined, 'Конфиденциальность'),
+    MenuRowData(Icons.date_range_outlined, 'Данные и память'),
+    MenuRowData(Icons.brush_outlined, 'Оформление'),
+    MenuRowData(Icons.language_outlined, 'Язык'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +26,15 @@ class UserProfile extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Настройки'),
       ),
-      body: const SizedBox(
+      body: SizedBox(
         width: double.infinity,
         child: Column(
           children: [
-            _UserInfo(),
-            SizedBox(height: 30),
-            _MenuWidget(),
+            const _UserInfo(),
+            const SizedBox(height: 30),
+            _MenuWidget(menuRows: firstMenuRows,),
+            const SizedBox(height: 30),
+            _MenuWidget(menuRows: secondMenuRows,),
           ],
         ),
       ),
@@ -25,28 +43,50 @@ class UserProfile extends StatelessWidget {
 }
 
 class _MenuWidget extends StatelessWidget {
-  const _MenuWidget({super.key});
+  const _MenuWidget({super.key, required this.menuRows});
+
+  final List<MenuRowData> menuRows;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
       color: Colors.white,
       width: double.infinity,
-      child: const Column(
+      child: Column(
+        children: menuRows.map((data) => _MenuWidgetRow(data: data)).toList(),
+      ),
+    );
+  }
+}
+
+class MenuRowData {
+  MenuRowData(this.icon, this.text);
+
+  final IconData icon;
+  final String text;
+}
+
+class _MenuWidgetRow extends StatelessWidget {
+  const _MenuWidgetRow({
+    super.key, required this.data,
+  });
+
+  final MenuRowData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Row(
         children: [
-          Row(
-            children: [
-              Icon(Icons.bookmark),
-              SizedBox(
-                width: 16,
-              ),
-              Expanded(
-                child: Text('Избранное'),
-              ),
-              Icon(Icons.chevron_right),
-            ],
-          )
+          Icon(data.icon),
+          const SizedBox(
+            width: 16,
+          ),
+          Expanded(
+            child: Text(data.text),
+          ),
+          Icon(Icons.chevron_right),
         ],
       ),
     );
